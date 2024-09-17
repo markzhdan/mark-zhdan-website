9f6e769f0556705c8dffacd9795e6a6dfb16af8a
Mark Zhdan <markzhdan@gmail.com>
2024-09-17 15:29:20 -0500

    Updated message statement

--- markzhdan/src/pages/Homepage/AboutMe/AboutMe.jsx
+++ markzhdan/src/pages/Homepage/AboutMe/AboutMe.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "../../../components/Link";
 
 const AboutMe = () => {
   return (
@@ -7,7 +8,9 @@ const AboutMe = () => {
 
       <p>
         I'm a full-stack web developer and a Computer Science student at the
-        University of Illinois Chicago. Looking for a summer 2024 internship.
+        University of Illinois Chicago. I made the VALORANT app{" "}
+        <Link name="Post-Plant" link="https://postplant.app/" /> to help users
+        improve.
       </p>
     </header>
   );
